f51b8d474575679f5cae872c6db48f334f36751a
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-10-19 20:07:08 +0300

    End of step 7

---
+++ src/gcd.js
@@ -0,0 +1,36 @@
+import { askQuestion, greeting } from './index.js';
+
+const getGCD = (a, b) => {
+    if (b) {
+        return getGCD(b, a % b);
+    }
+    return Math.abs(a);
+};
+
+const gcdGame = () => {
+    const name = greeting();
+
+    console.log('Find the greatest common divisor of given numbers.');
+
+    let wins = 0;
+
+    while (wins !== 3) {
+        const n1 = Math.round(Math.random() * 100);
+        const n2 = Math.round(Math.random() * 100);
+
+        const correctAnswer = getGCD(n1, n2);
+
+        const userAnswer = askQuestion(`${n1} ${n2}`);
+
+        if (parseInt(userAnswer, 10) === correctAnswer) {
+            wins += 1;
+            console.log('Correct!');
+        } else {
+            wins = 0;
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+        }
+    }
+    console.log(`Congratulations, ${name}!`);
+};
+
+export default gcdGame;
